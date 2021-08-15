6b0cf0b96051e43d12af6075805facdc1fe7e1dd
albertaljeanpierre <albertaljeanpierre@gmail.com>
2021-08-15 16:46:30 +0200

    Menu de navigation des catégories, page accueil et détail

--- base/fct_global.php
+++ base/fct_global.php
@@ -1,49 +1,52 @@
 <?php
 // affichage de debugage -> print_r mis en forme
-function print_q($val){
+function print_q($val)
+{
     echo "<pre style='background-color:#000;color:#3FBBD5;font-size:11px;z-index:99999;position:relative;'>";
     print_r($val);
     echo "</pre>";
 }
 
-function adminProtection(){
-    if((!isset($_SESSION["admin_id"])) || (empty($_SESSION["admin_id"]))|| (!is_numeric($_SESSION["admin_id"]))){
+function adminProtection()
+{
+    if ((!isset($_SESSION["admin_id"])) || (empty($_SESSION["admin_id"])) || (!is_numeric($_SESSION["admin_id"]))) {
         header("Location: index.php?p=login");
         exit;
     }
 }
 
-function adminMenu($public_menu = ""){
+function adminMenu($public_menu = "")
+{
     ?>
     <div class="row">
         <nav class="nav-show">
             <div class="container">
                 <ul>
                     <?php
-                    if(isset($_SESSION["admin_id"])){
-                    ?>
-                    <li><a href="index.php?p=gestion_item">Gestion des pages</a></li>
-                    <li><a href="index.php?p=gestion_admin">Gestion des utilisateurs</a></li>
-                    <li>
-                        <a>Gestion du shop</a>
-                        <ul>
-                            <li><a href="index.php?p=gestion_designer">Designer</a></li>
-                            <li><a href="index.php?p=gestion_manufacturer">Manufacturier</a></li>
-                            <li><a href="index.php?p=gestion_shape">Etat</a></li>
-                            <li><a href="index.php?p=gestion_categories">Catégories</a></li>
-                            <li><a href="index.php?p=gestion_sous-categories">Sous-catégories</a></li>
-                            <li><a href="index.php?p=gestion_fiches-produits">Fiches produits</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a>Mon compte</a>
-                        <ul>
-                            <li><a href="#">Modifier mon mot-de-passe</a></li>
-                            <li><a href="index.php?p=gestion_unlog">Déconnexion</a></li>
-                        </ul>
-                    </li>
-                    <?php
-                    }else {
+                    if (isset($_SESSION["admin_id"])) {
+                        ?>
+                        <li><a href="index.php?p=gestion_item">Gestion des pages</a></li>
+                        <li><a href="index.php?p=gestion_admin">Gestion des utilisateurs</a></li>
+                        <li>
+                            <a>Gestion du shop</a>
+                            <ul>
+                                <li><a href="index.php?p=gestion_designer">Designer</a></li>
+                                <li><a href="index.php?p=gestion_manufacturer">Manufacturier</a></li>
+                                <li><a href="index.php?p=gestion_shape">Etat</a></li>
+                                <li><a href="index.php?p=gestion_categories">Catégories</a></li>
+                                <li><a href="index.php?p=gestion_sous-categories">Sous-catégories</a></li>
+                                <li><a href="index.php?p=gestion_fiches-produits">Fiches produits</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a>Mon compte</a>
+                            <ul>
+                                <li><a href="#">Modifier mon mot-de-passe</a></li>
+                                <li><a href="index.php?p=gestion_unlog">Déconnexion</a></li>
+                            </ul>
+                        </li>
+                        <?php
+                    } else {
                         if (is_array($public_menu)) {
                             foreach ($public_menu as $pm) {
                                 $rm_id = $pm["item_id"];
@@ -59,6 +62,47 @@ function adminMenu($public_menu = ""){
         </nav>
     </div>
     <?php
+}
+
+/***
+ *  Fonction affichant un menu de navigation des catégories visibles
+ *
+ */
+function categorieMenu()
+{
+    $categorieList = getCategorieVisible();
+    // var_dump($categorieList);
+    ?>
+    <nav class="container" id="nav">
+        <ul class="row">
+            <?php
+            foreach ($categorieList as $categories) {
+
+                    echo "<li class=\"three columns\"><a href=\"./?p=category&id=1\" title=\"\">" . $categories['level_1'] . "</a></li>";
+
+            }
+            ?>
+            <!--            <li class="three columns"><a href="./?p=category&id=1" title="">Meubles</a></li>-->
+            <!--            <li class="three columns"><a href="./?p=category&id=49" title="">Luminaires</a></li>-->
+            <!--            <li class="three columns"><a href="./?p=category&id=56" title="">Accessoires</a></li>-->
+            <!--            <li class="three columns"><a href="./?p=category&id=79" title="">Les exceptionnels</a></li>-->
+        </ul>
+    </nav>
+    <?php
+}
+
+/**
+ * Fonction retournant un tableau des catégories visibles
+ * @return array|bool
+ */
+function getCategorieVisible()
+{
+// TODO Note il faut également récupérer la position en db de l'id de chaque catégorie
+    $sql = "SELECT   level_1 
+                FROM category_level_1  
+                WHERE `is_visible` = '1'
+                GROUP BY level_1 ASC;";
+    return requeteResultat($sql);
 }
 
 ?>
--- include/public_layout.php
+++ include/public_layout.php
@@ -1,33 +1,30 @@
-
 <body>
 <div class="main_content">
     <header class="u-full-width">
         <div class="container row">
             <h2 class="six columns" id="logo">
-                <a href="./" title=""><img src="images/content/logo.png" alt="" /></a>
+                <a href="./" title=""><img src="images/content/logo.png" alt=""/></a>
             </h2>
 
             <div class="six columns">
                 <form action="./" method="get">
-                    <input type="text" name="q" value="" placeholder="Que recherchez-vous ?" />
-                    <input type="hidden" name="p" value="search" />
-                    <input type="submit" name="submit" value="OK" />
+                    <input type="text" name="q" value="" placeholder="Que recherchez-vous ?"/>
+                    <input type="hidden" name="p" value="search"/>
+                    <input type="submit" name="submit" value="OK"/>
                 </form>
             </div>
         </div>
     </header>
-    <nav class="container" id="nav">
-        <ul class="row">
-            <li class="three columns"><a href="./?p=category&id=1" title="">Meubles</a></li>
-            <li class="three columns"><a href="./?p=category&id=49" title="">Luminaires</a></li>
-            <li class="three columns"><a href="./?p=category&id=56" title="">Accessoires</a></li>
-            <li class="three columns"><a href="./?p=category&id=79" title="">Les exceptionnels</a></li>
-        </ul>
-    </nav>
+
+            <?php
+            categorieMenu();
+
+            ?>
+
 
     <?php
-    $page_view = file_exists("view/".$page_view.".php") ? $page_view : exit("view non définie ou inexistante");
-    include_once("view/".$page_view.".php");
+    $page_view = file_exists("view/" . $page_view . ".php") ? $page_view : exit("view non définie ou inexistante");
+    include_once("view/" . $page_view . ".php");
     ?>
 
 
@@ -51,17 +48,17 @@
 
 </div>
 
-<link rel="stylesheet" type="text/css" href="css/public/normalize.css" media="screen" defer="true" />
-<link rel="stylesheet" type="text/css" href="css/public/skeleton.css" media="screen" defer="true" />
-<link rel="stylesheet" type="text/css" href="css/public/skeleton_collapse.css" media="screen" defer="true" />
-<link rel="stylesheet" type="text/css" href="css/admin/font-awesome.css" media="screen" defer="true" />
-<link rel="stylesheet" type="text/css" href="css/public/custom.css" media="screen" defer="true" />
+<link rel="stylesheet" type="text/css" href="css/public/normalize.css" media="screen" defer="true"/>
+<link rel="stylesheet" type="text/css" href="css/public/skeleton.css" media="screen" defer="true"/>
+<link rel="stylesheet" type="text/css" href="css/public/skeleton_collapse.css" media="screen" defer="true"/>
+<link rel="stylesheet" type="text/css" href="css/admin/font-awesome.css" media="screen" defer="true"/>
+<link rel="stylesheet" type="text/css" href="css/public/custom.css" media="screen" defer="true"/>
 
 <script src="js/ScatteredPolaroidsGallery/classie.js"></script>
 <script src="js/ScatteredPolaroidsGallery/photostack.js"></script>
 <script>
-    new Photostack( document.getElementById( 'photostack-1' ), {
-        callback : function( item ) {
+    new Photostack(document.getElementById('photostack-1'), {
+        callback: function (item) {
             //console.log(item)
         }
     });
